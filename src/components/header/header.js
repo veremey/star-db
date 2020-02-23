dcacd67448a5bbeda28accb7b786c4bb02eebc19
veremey <veremey4uk@gmail.com>
2020-02-23 14:08:57 +0200

    before get API data

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="header d-flex">
       <h3>
         <a href="#">
-          StarDB
+          Star DB
         </a>
       </h3>
       <ul className="d-flex">
